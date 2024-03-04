f46babae17d134b2aae4f03c25ba0b0efd415232
azimy <a.m.azimy@gmail.com>
2024-03-04 03:50:04 -0800

    cart develped

--- src/slices/cartSlice.js
+++ src/slices/cartSlice.js
@@ -5,6 +5,7 @@ const initialState={
     cartItems: localStorage.getItem('cartItems') ?
     JSON.parse(localStorage.getItem('cartItems')):
     [],
+    
     cartTotalQty:0,
     cartTotalAmount:0,
 }
